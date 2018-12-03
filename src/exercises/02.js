af160fd19becfcd47bae078b762cc0d46b860ef3
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2018-12-03 10:36:52 -0300

    basic compound component using React.Childre.map and React.cloneElement to pass props to static children elements

--- src/exercises/02.js
+++ src/exercises/02.js
@@ -16,6 +16,12 @@ class Toggle extends React.Component {
   //    be able to accept `on`, `toggle`, and `children` as props.
   //    Note that they will _not_ have access to Toggle instance properties
   //    like `this.state.on` or `this.toggle`.
+
+  static On = (props) => props.on ? props.children : null;
+  static Off = (props) => props.on ? null : props.children;
+
+  static Button = ({on, toggle}) => <Switch on={on} onClick={toggle} />;
+
   state = {on: false}
   toggle = () =>
     this.setState(
@@ -33,8 +39,16 @@ class Toggle extends React.Component {
     // 2. React.cloneElement: https://reactjs.org/docs/react-api.html#cloneelement
     //
     // 🐨 you'll want to completely replace the code below with the above logic.
-    const {on} = this.state
-    return <Switch on={on} onClick={this.toggle} />
+    // const {on} = this.state
+    // return <Switch on={on} onClick={this.toggle} />
+    return React.Children.map(this.props.children, childElement => {
+      // clone element is responsible for passing these compound components (static On, Off)
+      // the props they need to do their job
+      return React.cloneElement(childElement, {
+        on: this.state.on,        // clone react element and add extra props
+        toggle: this.toggle,
+      })
+    })
   }
 }
 
