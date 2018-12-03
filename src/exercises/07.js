f22297dbd794251126a71b7b3a72cfa50d93954b
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2018-12-03 15:45:42 -0300

    state initializer pattern

--- src/exercises/07.js
+++ src/exercises/07.js
@@ -7,16 +7,27 @@ const callAll = (...fns) => (...args) =>
   fns.forEach(fn => fn && fn(...args))
 
 class Toggle extends React.Component {
+
+  static defaultProps = {
+    initialOn: false,
+  }
   // 🐨 We're going to need some static defaultProps here to allow
   // people to pass a `initialOn` prop.
   //
   // 🐨 Rather than initializing state to have on as false,
   // set on to this.props.initialOn
-  state = {on: false}
+  initialState = {on: this.props.initialOn};
+  state = this.initialState
 
   // 🐨 now let's add a reset method here that resets the state
   // to the initial state. Then add a callback that calls
   // this.props.onReset with the `on` state.
+  reset = () => {
+    this.setState(
+      this.initialState, 
+      () => this.props.onReset(this.state.on) // callback
+    )
+  }
   toggle = () =>
     this.setState(
       ({on}) => ({on: !on}),
@@ -35,6 +46,7 @@ class Toggle extends React.Component {
       toggle: this.toggle,
       // 🐨 now let's include the reset method here
       // so folks can use that in their implementation.
+      reset: this.reset,
       getTogglerProps: this.getTogglerProps,
     }
   }
